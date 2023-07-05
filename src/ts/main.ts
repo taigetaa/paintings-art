49bcea26dcc1f4a3dd422f41431bfb6ee700b7bc
Victoria <bethmorgendorffer@mail.ru>
2023-07-05 21:58:37 +0300

    calculator

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,6 +4,7 @@ import forms from "./modules/forms";
 import mask from "./modules/mask";
 import checkTextInputs from "./modules/checkTextInputs";
 import showMoreStyles from "./modules/showMoreStyles";
+import calc from "./modules/calc";
 
 window.addEventListener('DOMContentLoaded', () : void => {
     modals();
@@ -14,4 +15,5 @@ window.addEventListener('DOMContentLoaded', () : void => {
     checkTextInputs('[name="name"]');
     checkTextInputs('[name="message"]');
     showMoreStyles('.button-styles', '.styles-2');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
 });
