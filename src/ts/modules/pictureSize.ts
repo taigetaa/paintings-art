de87572ce610f22b1733c4325d959437deb78961
Victoria <bethmorgendorffer@mail.ru>
2023-07-12 13:31:26 +0300

    images

---
+++ src/ts/modules/pictureSize.ts
@@ -0,0 +1,41 @@
+const pictureSize = (imgSelector: string) => {
+  const blocks = document.querySelectorAll(imgSelector);
+
+  const showImg = (block: Element) => {
+    const img = block!.querySelector("img");
+    if (img) {
+      const paragraphs: NodeListOf<HTMLElement> | null = block.querySelectorAll("p:not(.size-hit)");
+      img.src = img.src.slice(0, -4) + "-1.png";
+      paragraphs.forEach(p => {
+        if (p) {
+          p.style.display = "none";
+        }
+      });
+    }
+  };
+
+  const hideImg = (block: Element) => {
+    const img = block.querySelector("img");
+    if (img) {
+      const paragraphs: NodeListOf<HTMLElement> | null = block.querySelectorAll("p:not(.size-hit)");
+      img.src = img.src.slice(0, -6) + ".png";
+      paragraphs.forEach((p) => {
+        if (p) {
+          p.style.display = "block";
+        }
+      });
+    }
+  };
+
+  blocks.forEach(block => {
+    block.addEventListener("mouseover", () => {
+      showImg(block);
+    });
+
+    block.addEventListener("mouseout", () => {
+      hideImg(block);
+    });
+  });
+};
+
+export default pictureSize;
