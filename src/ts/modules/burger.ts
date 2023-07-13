101aec110c34e5c1c953c5ca370a7b144bad023a
Victoria <bethmorgendorffer@mail.ru>
2023-07-13 21:37:06 +0300

    burger menu

--- src/ts/modules/burger.ts
+++ src/ts/modules/burger.ts
@@ -1,12 +1,13 @@
 const burger = (menuSelector: string, burgerSelector: string) => {
   const menuElem: HTMLElement | null = document.querySelector(menuSelector);
   const burgerElem: HTMLElement | null = document.querySelector(burgerSelector);
+  const maxSize: number = 992;
 
   if (menuElem && burgerElem) {
     menuElem.style.display = "none";
 
     burgerElem.addEventListener("click", () => {
-        if (menuElem.style.display == 'none' && window.screen.availWidth < 993) {
+        if (menuElem.style.display == 'none' && window.screen.availWidth <= maxSize) {
             menuElem.style.display = "block";
         } else {
             menuElem.style.display = "none";
@@ -15,7 +16,7 @@ const burger = (menuSelector: string, burgerSelector: string) => {
   }
 
   window.addEventListener('recize', () => {
-    if (menuElem && burgerElem && window.screen.availWidth > 992) {
+    if (menuElem && burgerElem && window.screen.availWidth > maxSize) {
         menuElem.style.display = 'none';
     }
   });
